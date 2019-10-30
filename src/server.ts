82aae54811fcae6c76e5428bd7f9ba8db024dcb2
William Sedlacek <wsedlacekc@gmail.com>
2019-10-30 12:30:04 -0500

    Auth
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,23 @@ import { Middleware } from './models';
 import { hubsRouter } from './routes/hubs.route';
 
 export const server = express();
+
 export const logger: Middleware = () => (req, res, next) => {
   console.log(`\n${new Date().toISOString()} ${req.method} ${req.url}\n`);
   next();
 };
 
+export const gateKeeper: Middleware = () => (req, res, next) => {
+  const password = (req.headers.password as string) || '';
+  if (!password) res.status(400).json({ message: 'A password header is required' });
+  else if (password.toLowerCase() !== 'mellon') res.status(401).json({ you: 'shall not pass!!' });
+  else return next();
+};
+
 server.use(express.json());
 server.use(helmet());
 server.use(logger());
+server.use(gateKeeper());
 
 server.use('/api/hubs', hubsRouter);
 
